2854ac3e8a5fd3dcee8d1ac1e6f631af2ea16d13
Michał Jaremczuk <michaljaremczuk@gmail.com>
2026-06-04 13:36:27 +0200

    updates before release

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,16 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  site: 'https://mjaremczuk.github.io',
   integrations: [react()],
 
   vite: {
